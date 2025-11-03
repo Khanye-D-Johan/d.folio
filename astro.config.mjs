f17b42ba401ec267c788d4ab4a0b0e0bc94ae6c0
johan-khanye <johan.khanye@gmail.com>
2025-11-04 00:31:43 +0200

    Add Offering/Services Catalogue Page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://khanyej.netlify.app/",
 	integrations: [
-		mdx(), sitemap(), preact()
+		icon(), mdx(), sitemap(), preact()
 	],
 });
